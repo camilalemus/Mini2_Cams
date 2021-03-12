7bc5ba4cf641f9ff367fc2e1ad6c914ae09af900
camilalemus <lem18272@uvg.edu.gt>
2021-03-11 22:08:02 -0600

    I2C Solve Errors

--- Mini2.X/nbproject/Makefile-default.mk
+++ Mini2.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=mainm2.c I2C.c
+SOURCEFILES_QUOTED_IF_SPACED=mainm2.c I2C.c BMP280.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/mainm2.p1 ${OBJECTDIR}/I2C.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/mainm2.p1.d ${OBJECTDIR}/I2C.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/mainm2.p1 ${OBJECTDIR}/I2C.p1 ${OBJECTDIR}/BMP280.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/mainm2.p1.d ${OBJECTDIR}/I2C.p1.d ${OBJECTDIR}/BMP280.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/mainm2.p1 ${OBJECTDIR}/I2C.p1
+OBJECTFILES=${OBJECTDIR}/mainm2.p1 ${OBJECTDIR}/I2C.p1 ${OBJECTDIR}/BMP280.p1
 
 # Source Files
-SOURCEFILES=mainm2.c I2C.c
+SOURCEFILES=mainm2.c I2C.c BMP280.c
 
 
 
@@ -110,6 +110,14 @@ ${OBJECTDIR}/I2C.p1: I2C.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/I2C.d ${OBJECTDIR}/I2C.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/I2C.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/BMP280.p1: BMP280.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/BMP280.p1.d 
+	@${RM} ${OBJECTDIR}/BMP280.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/BMP280.p1 BMP280.c 
+	@-${MV} ${OBJECTDIR}/BMP280.d ${OBJECTDIR}/BMP280.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/BMP280.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/mainm2.p1: mainm2.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
@@ -126,6 +134,14 @@ ${OBJECTDIR}/I2C.p1: I2C.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/I2C.p1 I2C.c 
 	@-${MV} ${OBJECTDIR}/I2C.d ${OBJECTDIR}/I2C.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/I2C.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/BMP280.p1: BMP280.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/BMP280.p1.d 
+	@${RM} ${OBJECTDIR}/BMP280.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/BMP280.p1 BMP280.c 
+	@-${MV} ${OBJECTDIR}/BMP280.d ${OBJECTDIR}/BMP280.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/BMP280.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
